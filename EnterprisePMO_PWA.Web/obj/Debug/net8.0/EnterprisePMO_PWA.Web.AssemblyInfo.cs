2c809b6921c696390d8aeb01639a2dde0aaa9ec6
LenoreWoW <hassan.a.alsahli@hotmail.com>
2025-03-31 14:13:21 +0300

    0.1.3

--- EnterprisePMO_PWA.Web/obj/Debug/net8.0/EnterprisePMO_PWA.Web.AssemblyInfo.cs
+++ EnterprisePMO_PWA.Web/obj/Debug/net8.0/EnterprisePMO_PWA.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnterprisePMO_PWA.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e5bfc51b7bb8c0aba9b02d91f19d28d96312928")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b27d113117e1eb34c6eb5865d1a54b90142afa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnterprisePMO_PWA.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnterprisePMO_PWA.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
